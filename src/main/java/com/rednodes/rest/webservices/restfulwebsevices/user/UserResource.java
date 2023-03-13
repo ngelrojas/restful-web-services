4fc3bd549746020647451df5a3578257d50a026a
ngel rojas <aomarrojasp@gmail.com>
2023-03-13 16:30:03 -0300

    add: adding delete method users

--- src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserResource.java
+++ src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserResource.java
@@ -40,4 +40,9 @@ public class UserResource {
         return ResponseEntity.created(location).build();
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
+
 }
