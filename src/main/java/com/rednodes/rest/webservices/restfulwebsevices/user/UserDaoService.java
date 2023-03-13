bde2a776f2ede7ee77f53d8c4b2df0bf22326013
ngel rojas <aomarrojasp@gmail.com>
2023-03-13 16:24:02 -0300

    add: adding custom exception handler for errors http

--- src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserDaoService.java
+++ src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserDaoService.java
@@ -11,18 +11,25 @@ import java.util.function.Predicate;
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
 
-    static {
-        users.add(new User(1, "angel", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "nelson", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "omar", LocalDate.now().minusYears(29)));
-    }
+    private static int usersCount = 0;
 
+    static {
+        users.add(new User(++usersCount, "angel", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "nelson", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "omar", LocalDate.now().minusYears(29)));
+    }
     public List<User> findAll() {
         return users;
     }
 
     public User findOne(int id){
         Predicate<? super User> predicate = user -> user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setId(usersCount);
+        users.add(user);
+        return user;
     }
 }
--- src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserResource.java
+++ src/main/java/com/rednodes/rest/webservices/restfulwebsevices/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.rednodes.rest.webservices.restfulwebsevices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +23,21 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saveUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
 
 }
