9c41cf8975a4cd14ee8baa284e476cd76318f0fb
ngel rojas <aomarrojasp@gmail.com>
2023-03-14 14:48:37 -0300

    add: adding internationalization

--- src/main/java/com/rednodes/rest/webservices/restfulwebsevices/helloworld/HelloWorldController.java
+++ src/main/java/com/rednodes/rest/webservices/restfulwebsevices/helloworld/HelloWorldController.java
@@ -1,13 +1,19 @@
 package com.rednodes.rest.webservices.restfulwebsevices.helloworld;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.MessageSource;
 
-import com.rednodes.rest.webservices.restfulwebsevices.helloworld.HelloWorldBean;
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String HelloWorld() {
         return "Hello World";
@@ -22,5 +28,12 @@ public class HelloWorldController {
         return new HelloWorldBean(
                 String.format("Hello World, %s", name)
         );
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message",locale);
+
     }
 }
