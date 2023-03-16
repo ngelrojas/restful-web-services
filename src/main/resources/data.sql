93b85581dcf12a285e1b4406395bec087c0f38b4
ngel rojas <aomarrojasp@gmail.com>
2023-03-16 14:13:24 -0300

    add: adding connection with h2 and jpa, hibernet

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id,birth_date,name) values (1002, current_date(), 'Angel');
+insert into user_details(id,birth_date,name) values (1003, current_date(), 'Omar');
+insert into user_details(id,birth_date,name) values (1004, current_date(), 'Pamela');
